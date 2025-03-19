6de6f85c6b73f3ad7b94a5991355b362cbc714ef
Amanuel Belete <amanuelbelete95@gmail.com>
2025-03-19 16:06:15 +0300

    refactor: refactor the setup

--- index.js
+++ index.js
@@ -14,10 +14,15 @@ app.get("/", (req, res) => {
 })
 
 
-app.listen(4000, () => {
-    console.log("Server is running")
-})
- 
-mongoose.connect('mongodb+srv://emmanuelbelete7:<r5CSMiwn8YTD5Wd>@addisbackend-db.zukjs.mongodb.net/?retryWrites=true&w=majority&appName=AddisBackend-DB')
+// connection string
+const MONGODB_URI = "mongodb+srv://emmanuelbelete7:r5CSMiwn8YTD5Wd@addisbackend-db.zukjs.mongodb.net/?retryWrites=true&w=majority&appName=AddisBackend-DB"
+// port running on the backend
+const PORT = 4000;
 
-
+// Connect to MongoDB
+mongoose.connect(MONGODB_URI).then(()=>{
+    console.log("MongoDB Connected Successfully");
+    app.listen(PORT, async () => {
+        console.log(`Server running on port ${PORT}`); 
+    });
+}).catch((err)=> console.log("Connection Error", err))
