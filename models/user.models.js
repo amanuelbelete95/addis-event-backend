62656ac0e67e6f492ee9b1605fd29d1fb4fb3d52
Amanuel Belete <amanuelbelete95@gmail.com>
2025-06-17 16:02:18 +0300

    Add user routes

---
+++ models/user.models.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+},
+    {
+        timestamps: true
+    }
+)
+
+export const user = mongoose.models('User', userSchema )
