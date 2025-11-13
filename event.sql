3f055b9582f647d1a5f6f9693d1253c28722362d
Amanuel Belete <amanuelbelete95@gmail.com>
2025-11-13 22:31:13 +0300

    Add formatter

--- event.sql
+++ event.sql
@@ -1,18 +1,28 @@
-CREATE DATABASE events;
+create database events;
 
-CREATE TABLE event (
-    event_id SERIAL PRIMARY KEY,
-    name VARCHAR(200),
-    event_date DATE,
-    event_status VARCHAR(200),
-    description TEXT
+create table event (
+  event_id serial primary key,
+  name VARCHAR(200),
+  event_date date,
+  event_status VARCHAR(200),
+  description TEXT
 );
 
-ALTER TABLE event
-ADD COLUMN location VARCHAR(200);
+alter table event
+add column location VARCHAR(200);
 
+insert into
+  event (
+    name,
+    event_date,
+    event_status,
+    description,
+    location
+  )
+values
+  ();
 
-INSERT INTO event (
-    name, event_date, event_status, description, location
-)
-VALUES ()
+select
+  *
+from
+  event;
