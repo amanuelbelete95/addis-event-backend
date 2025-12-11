1cb60d2144f67b358e4567cfbf807cab839ab391
Amanuel Belete <amanuelbelete95@gmail.com>
2025-12-11 14:27:05 +0300

    fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,15 +4,11 @@ import jwt from 'jsonwebtoken';
 import pool from '../db.js';
 
 
-const generateToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
-}
-
-const createUser = async (userName, password) => {
+const createUser = async (userName, password, role) => {
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = await pool.query(
         'INSERT INTO users (userName password) VALUES ($1, $2, $3) RETURNING *',
-        [userName, hashedPassword]
+        [userName, hashedPassword, role]
     );
     return newUser.rows[0];
 };
@@ -27,13 +23,13 @@ export const registerUser = async (req, res) => {
             });
         }
 
-        const user = await pool.query('SELECT * FROM users WHER= $1',);
+        const user = await pool.query('SELECT * FROM users WHERE= $1', [userName]);
         if (user.rows.length > 0) {
             return res.status(400).json({ message: 'User already exists' });
         }
         // Register the new User
-        const newUser = await createUser(userName, password);
-        res.status(201).json({ message: 'User created successfully' });
+        const newUser = await createUser(userName, password, role);
+        res.status(201).json({ message: `User with ${newUser.userName} created successfully`, user: newUser });
     } catch (error) {
         console.error('Error creating user:', error);
         res.status(500).json({ message: 'Internal server error' });
@@ -58,9 +54,8 @@ export const logInUser = async (req, res) => {
             res.status(400).json({ message: "InValid credential" })
         }
 
-        const token = generateToken(user.id)
-
-        return res.status(201).json({ message: `Welcome back, ${userName}` })
+        const token = jwt.sign({ id: user.id, role: user.role }, process.env.JWT_SECRET, { expiresIn: "1h" })
+        return res.status(200).json({ token })
     } catch (error) {
         res.status(500).json({ message: 'Internal server error' });
     }
